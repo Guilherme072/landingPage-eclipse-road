414f41c60a2225cf73241b87c983ad4b54873d84
Guilherme Vieira <111805132+Guilherme072@users.noreply.github.com>
2024-12-06 19:39:42 -0300

    Delete js directory

--- js/hamburger-agenciados.js
+++
@@ -1,8 +0,0 @@
-const hamburger = document.getElementById('hamburger');
-const overlay = document.getElementById('overlay');
-
-// Quando o hambúrguer for clicado, alterna a classe 'active' na navbar
-hamburger.addEventListener('click', () => {
-  // Alterna a classe 'active' para a navbar, o que fará a overlay descer
-  document.querySelector('.navbar').classList.toggle('active');
-});
--- js/startArrow.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-
-    document.addEventListener("scroll", () => {
-        document.querySelector("#seta-inicio img").style.animation = "start-arrow-remove 1s ease forwards";
-    })
-
-})
