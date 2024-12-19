675d54cfc28249e32276fab6636cf764e88d8652
Guilherme Vieira <111805132+Guilherme072@users.noreply.github.com>
2024-12-19 16:40:03 -0300

    Add files via upload

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -1,11 +1,11 @@
 document.addEventListener("DOMContentLoaded", function() {
-    var navLinks = document.querySelectorAll('.nav-link');
-    var currentURL = window.location.href;
+  var navLinks = document.querySelectorAll('.nav-link');
+  var currentPath = window.location.pathname; // Obtém o caminho da URL atual (ex: /sobre-nos)
   
-    navLinks.forEach(function(link) {
-      if (currentURL.includes(link.getAttribute('href'))) {
-        link.classList.add('active'); // Adiciona a classe 'active' para indicar a aba ativa
-      }
-    });
+  navLinks.forEach(function(link) {
+    var linkPath = new URL(link.href).pathname; // Converte o href em caminho absoluto
+    if (currentPath === linkPath) {
+      link.classList.add('active'); // Adiciona a classe 'active' para indicar a aba ativa
+    }
   });
-  
+});
