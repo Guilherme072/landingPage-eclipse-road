000b51b72446d22c85d7d7a2b7b5694328377f34
Guilherme <cunha.guile@gmail.com>
2025-01-25 23:36:46 -0300

    top

---
+++ assets/js/startArrow.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+    document.addEventListener("scroll", () => {
+        document.querySelector("#seta-inicio img").style.animation = "start-arrow-remove 1s ease forwards";
+    })
+
+})
