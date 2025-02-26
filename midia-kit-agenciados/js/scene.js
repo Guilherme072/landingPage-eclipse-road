32d0f61ed9041c81b9c132407fa1fa604666ea83
Guilherme <cunha.guile@gmail.com>
2025-02-25 22:20:10 -0300

    subi aqui

--- midia-kit-agenciados/js/scene.js
+++ midia-kit-agenciados/js/scene.js
@@ -1,25 +1,33 @@
 document.addEventListener("DOMContentLoaded", function () {
-    lottie.loadAnimation({
-      container: document.getElementById("scene"), 
-      renderer: "svg", // 
-      loop: true, 
-      autoplay: true, 
-      path: "/icon-json/Scene.json" 
-    });
+  lottie.loadAnimation({
+    container: document.getElementById("scene"),
+    renderer: "svg", // 
+    loop: true,
+    autoplay: true,
+    path: "/icon-json/Scene.json"
+  });
 
-    lottie.loadAnimation({
-        container: document.getElementById("profilee"), 
-        renderer: "svg",
-        loop: true,
-        autoplay: true,
-        path: "/icon-json/Profile.json" 
-      });
-    
-      lottie.loadAnimation({
-        container: document.getElementById("push"), 
-        renderer: "svg",
-        loop: true,
-        autoplay: true,
-        path: "/icon-json/Push-notification.json" 
-      });
+  lottie.loadAnimation({
+    container: document.getElementById("profilee"),
+    renderer: "svg",
+    loop: false, //pra parar o loop - Gabs
+    autoplay: true,
+    path: "/icon-json/Profile.json"
+  });
+
+  lottie.loadAnimation({
+    container: document.getElementById("push"),
+    renderer: "svg",
+    loop: true,
+    autoplay: true,
+    path: "/icon-json/Push-notification.json"
+  });
+
+  lootie.loadAnimation({
+    container: document.getElementById("celular"),
+    renderer: "svg",
+    loop: true,
+    autoplay: true,
+    path: "/icon-json/celular.json"
+  });
 });
