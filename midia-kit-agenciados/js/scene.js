66f16a2ae21858f950865fb377810ac37cb90a8b
Guilherme <cunha.guile@gmail.com>
2025-02-26 22:29:46 -0300

    mds senhor farofa

--- midia-kit-agenciados/js/scene.js
+++ midia-kit-agenciados/js/scene.js
@@ -1,7 +1,8 @@
 document.addEventListener("DOMContentLoaded", function () {
+  // Animações que devem rodar normalmente
   lottie.loadAnimation({
     container: document.getElementById("scene"),
-    renderer: "svg", // 
+    renderer: "svg",
     loop: true,
     autoplay: true,
     path: "/icon-json/Scene.json"
@@ -10,16 +11,34 @@ document.addEventListener("DOMContentLoaded", function () {
   lottie.loadAnimation({
     container: document.getElementById("profilee"),
     renderer: "svg",
-    loop: false, //pra parar o loop - Gabs
+    loop: false,
     autoplay: true,
     path: "/icon-json/Profile.json"
   });
 
-  lottie.loadAnimation({
-    container: document.getElementById("push"),
-    renderer: "svg",
-    loop: false,
-    autoplay: true,
-    path: "/icon-json/Push-notification.json"
-  });
+  // Função para ativar a animação do #push quando ele aparecer na tela
+  const pushAnimation = () => {
+    lottie.loadAnimation({
+      container: document.getElementById("push"),
+      renderer: "svg",
+      loop: false,
+      autoplay: true,
+      path: "/icon-json/Push-notification.json"
+    });
+  };
+
+  // Cria um observador de interseção para o elemento #push
+  const observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        // Ativa a animação quando o elemento entra na tela
+        pushAnimation();
+        observer.unobserve(entry.target); // Interrompe o observador após ativar a animação
+      }
+    });
+  }, { threshold: 0.5 }); // 50% do elemento precisa estar visível
+
+  // Começa a observar o elemento #push
+  const pushElement = document.getElementById("push");
+  observer.observe(pushElement);
 });
